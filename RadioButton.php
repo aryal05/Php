fb9967c432be71beb9fe229f56265734f0d2c798
aryal05 <aryal.rajat05@gmail.com>
2024-08-24 21:24:35 +0545

    checkbox

--- RadioButton.php
+++ RadioButton.php
@@ -7,13 +7,23 @@
 </head>
 <body>
     <form action="RadioButton.php" method="post">
-        <label for="">Select Course</label><br>
+        <!-- <label for="">Select Course</label><br>
         <input type="radio" value="Bsc" name="course"> Bsc <br>
         <input type="radio" value="BscCsit" name="course"> BscCsit <br>
         <input type="radio" value="BIT" name="course"> BIT <br>
 
         <input type="submit" name="confirm" value="Confirm">
+ -->
 
+
+       <label for=""><b>Food Items</b></label> <br>
+       <input type="checkbox" name="foods[]" value="FastFood">FastFood <br>
+       <input type="checkbox" name="foods[]" value="Sweets"> Sweets <br>
+       <input type="checkbox" name="foods[]" value="Namkeen"> Namkeen <br>
+       <input type="checkbox" name="foods[]" value="Vegies"> Vegies <br>
+        
+
+       <input type="submit" value="Confirm" name="confirm">
 
 
 
@@ -21,11 +31,15 @@
 
 
     <?php
-       if(isset($_POTS['confim'])){
-        $course = $_POST['course'];
-        echo $course;
-       }
+    //    if(isset($_POST['confirm'])){
+    //     $course = $_POST['course'];
+    //     echo $course;
+    //    }
     
+   if(isset($_POST['confirm'])){
+    $foods = $_POST['foods'];
+    echo $foods;
+   }
     
     ?>
 </body>
