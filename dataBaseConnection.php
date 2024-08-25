8c3f83f6cd364e09c2ba2a78bf1bc0f33e273571
aryal05 <aryal.rajat05@gmail.com>
2024-08-25 12:31:03 +0545

    connecting databse by oops

--- dataBaseConnection.php
+++ dataBaseConnection.php
@@ -8,6 +8,25 @@
 
 */
 
+// Connectiong MySQL with  (oops)
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbName = 'php';
+
+
+//Create Connection
+$conn = new mysqli($servername,$username, $password, $dbName);
+
+
+// Check connection
+
+if($conn->connect_error){
+    echo 'Conection failed:' . $conn-> connect_error;
+    exit;
+}
+
+echo 'Connection Sucessful';
 
 
 ?>
