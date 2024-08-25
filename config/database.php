42b55999525b872a6b986b6d45a8cd8c04d7d356
aryal05 <aryal.rajat05@gmail.com>
2024-08-25 12:57:52 +0545

    inssert connection

---
+++ config/database.php
@@ -0,0 +1,42 @@
+<?php 
+
+/* 
+   //DataBase Connection
+   MySQli extention - Works with only MySQL Dtabase
+   PDO(PHP Data Object) - Work with 12 Different Databases 
+
+
+*/
+
+// Connectiong MySQL with  (oops)
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbName = 'php';
+
+
+//Create Connection by oops
+// $conn = new mysqli($servername,$username, $password, $dbName);
+
+
+//Connecting with procedural approach 
+$conn = mysqli_connect($servername, $username, $password, $dbName);
+
+if($conn){
+    echo 'Connected Successfully';
+}else{
+    echo'Filed to connect, error:' . mysqli_connect_error();
+}
+
+
+// Check connection
+
+// if($conn->connect_error){
+//     echo 'Conection failed:' . $conn-> connect_error;
+//     exit;
+// }
+
+// echo 'Connection Sucessful';
+
+
+?>
--- dataBaseConnection.php
+++ dataBaseConnection.php
@@ -1,42 +1,12 @@
 <?php 
+ // Insert Operation
 
-/* 
-   //DataBase Connection
-   MySQli extention - Works with only MySQL Dtabase
-   PDO(PHP Data Object) - Work with 12 Different Databases 
+ include ('config/database.php')
 
 
-*/
-
-// Connectiong MySQL with  (oops)
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbName = 'php';
 
 
-//Create Connection by oops
-// $conn = new mysqli($servername,$username, $password, $dbName);
 
-
-//Connecting with procedural approach 
-$conn = mysqli_connect($servername, $username, $password, $dbName);
-
-if($conn){
-    echo 'Connected Successfully';
-}else{
-    echo'Server Error Not Connected';
-}
-
-
-// Check connection
-
-// if($conn->connect_error){
-//     echo 'Conection failed:' . $conn-> connect_error;
-//     exit;
-// }
-
-// echo 'Connection Sucessful';
 
 
 ?>
