7ba98a8298a2fd8280bb8ba3ef500c95e741b349
aryal05 <aryal.rajat05@gmail.com>
2024-08-26 20:32:03 +0545

    signup form completion

--- Registration/signup.php
+++ Registration/signup.php
@@ -11,24 +11,23 @@
 
 <div class="container">
     <h1 class="text-center">Sign Up To Our Websites</h1>
-    <form>
+    <form action="/PHP/Registration/signup.php" method="post">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Usernames</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="username" class="form-label">Usernames</label>
+    <input type="email" class="form-control" id="username" name="username" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="cpassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+    <div id="emailHelp" class="form-text">Make sure you enter the same passsword.</div>
+   
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
+ 
   <button type="submit" class="btn btn-primary">SignUp</button>
 </form>
 </div>
