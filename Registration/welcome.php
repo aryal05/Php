14f900ea48e3002895eb717fa9b9e00e07fea8ba
aryal05 <aryal.rajat05@gmail.com>
2024-08-28 19:12:31 +0545

    login redirect to home

--- Registration/welcome.php
+++ Registration/welcome.php
@@ -1,16 +1,27 @@
+<?php
+session_start();
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!= true){
+    header("location:login.php");
+    exit;
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Welcome  <?php echo $_SESSION['username']  ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 <?php include('partials/_nav.php'); ?>
 
-
-
+Welcome <?php echo$_SESSION['username']  ?>
+<h1>Welcome to home </h1>
 
 
 
