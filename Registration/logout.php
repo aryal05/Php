e7e5f27940eedf25f99f3371e9684317914c36be
aryal05 <aryal.rajat05@gmail.com>
2024-08-29 20:12:52 +0545

    logout start up

---
+++ Registration/logout.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+
+
+if(!isset($_SESSION['loggedin'])|| $_SESSION['loggedin'] != true){
+    header(
+        "location: login.php"
+        );
+    exit;
+}else{
+    echo 'error occured and cannot reodirect to the welcome page';
+}
+
+
+?>
