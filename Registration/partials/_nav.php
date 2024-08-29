75dce28f2e0e22c6bea9c6150429ddef32f49ed6
aryal05 <aryal.rajat05@gmail.com>
2024-08-29 20:45:26 +0545

    login system donr hash passwod remains

--- Registration/partials/_nav.php
+++ Registration/partials/_nav.php
@@ -1,7 +1,4 @@
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +8,12 @@
 </head>
 <body>
   <?php
-   session_start();
-   if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']== true){
+  session_start();
+   if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']== true){
     $loggedin = true;
-};
+}else{
+  $loggedin = false;
+}
 
 
 
@@ -28,18 +27,23 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/PHP/Registration/welcome.php">Home</a>
-        </li>
-        <li class="nav-item">
+        </li>';
+        if(!$loggedin){
+        echo '<li class="nav-item">
           <a class="nav-link" href="/PHP/Registration/login.php">Login</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/PHP/Registration/signup.php">SignUp</a>
-        </li>
-        <li class="nav-item">
+        </li>';
+        }
+
+        if($loggedin){
+        echo '<li class="nav-item">
           <a class="nav-link" href="/PHP/Registration/logout.php">LogOut</a>
-        </li>
+        </li>';
+        }
       
-      </ul>
+      echo '</ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
